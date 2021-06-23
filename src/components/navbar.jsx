533301fc5e3e816e9d7d859b819a0955835e25ce
lorlahdey <horlorlahdey33@gmail.com>
2021-06-23 22:58:48 +0100

    menu navigation

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react'
 
 function Navbar() {
-    
+    const [login, setlogin] = useState("")
 
 	return (
-		<nav className='navbar'> 
-            <Link className='link' to='/login'>Login </Link>
-            <Link className='link' to='/register'>Register</Link>
-            <Link className='link' to='/my-note'>My Note</Link>
+		// <nav className='navbar'> 
+        //     <Link className='link' to='/login'>Login </Link>
+        //     <Link className='link' to='/register'>Register</Link>
+        //     <Link className='link' to='/my-note'>My Note</Link>
 			
+		// </nav>
+
+        //working with specific menu for each page on the nav bar
+        <nav className='navbar'> 
+			{login ? (
+				<>
+					<Link className='link' to='/my-note'>My List</Link>
+					<br />
+					<span>Logout</span>
+				</>
+			) : (
+				<>
+					<Link className='link' to='/login'>Login</Link>
+					<br />
+					<Link className='link' to='/register'>Register</Link>
+					<br />
+				</>
+			)}
 		</nav>
-        
 	);
 }
 
